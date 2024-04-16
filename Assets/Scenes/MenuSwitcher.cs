3a363798d9beb79a34a7a1fa187714f2267affd4
Matěj Boška <matej0boska@gmail.com>
2024-04-16 14:05:56 +0200

    Added map scene and terrains

---
+++ Assets/Scenes/MenuSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSwitcher : MonoBehaviour {
+
+	private Animator anim;
+
+	void Start()
+	{
+		this.anim = GetComponent<Animator>();
+	}
+
+	public void SetAnimator(int i)
+	{
+		this.anim.SetInteger("SwitchToMenu", i);
+	}
+}
